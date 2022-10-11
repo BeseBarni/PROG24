bce94eff64869fe667a1f0e1671dc8f3cdb8dfbd
Barnabás Bese <besebarni@gmail.com>
2022-10-11 18:05:26 +0200

    socket

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import socket
+from threading import *
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -16,5 +18,33 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 ev3 = EV3Brick()
 
 
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "127.0.0.1"
+port = 8000
+print (host)
+print (port)
+serversocket.bind((host, port))
+
+class client(Thread):
+    def __init__(self, socket, address):
+        Thread.__init__(self)
+        self.sock = socket
+        self.addr = address
+        self.start()
+
+    def run(self):
+        while 1:
+            print(self.address)
+            self.sock.send(b'Oi you sent something to me')
+
+serversocket.listen(5)
+print ('server started and listening')
+while 1:
+    clientsocket, address = serversocket.accept()
+    client(clientsocket, address)
+    
+
 # Write your program here.
-ev3.speaker.beep()
+
+
